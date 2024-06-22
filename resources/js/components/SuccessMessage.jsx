1527913d2283d4067e43ec2141b97327c6ab283d
Teodor469 <lifetroll6@gmail.com>
2024-06-22 12:48:40 +0300

    added the closing functionality of the pop-up message

--- resources/js/components/SuccessMessage.jsx
+++ resources/js/components/SuccessMessage.jsx
@@ -1,22 +1,35 @@
 import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
 
 const SuccessMessage = ({ message }) => {
-  const [visible, setVisible] = useState(true);
+    const [visible, setVisible] = useState(true);
 
-  const handleClose = () => {
-    setVisible(false);
-  };
+    const handleClose = () => {
+        setVisible(false);
+    };
 
-  if (!visible) return null;
+    if (!visible) return null;
 
-  return (
-    <div className="bg-green-500 text-white font-bold px-4 py-2 rounded-md shadow-md transition-all duration-150 ease-in-out" role="alert">
-      {message}
-      <button type="button" className="top-0 right-0 px-3 py-1 text-white" onClick={handleClose} aria-label="Close">
-        X
-      </button>
-    </div>
-  );
+    return (
+        <div className="bg-green-500 text-white font-bold px-4 py-2 rounded-md shadow-md transition-all duration-150 ease-in-out" role="alert">
+            {message}
+            <button type="button" className="top-0 right-0 px-3 py-1 text-white" onClick={handleClose} aria-label="Close">
+                X
+            </button>
+        </div>
+    );
 };
 
 export default SuccessMessage;
+
+if (document.getElementById('success-message')) {
+    const element = document.getElementById('success-message');
+    const message = element.getAttribute('data-message');
+    const root = ReactDOM.createRoot(element);
+
+    root.render(
+        <React.StrictMode>
+            <SuccessMessage message={message} />
+        </React.StrictMode>
+    );
+}
--- resources/views/shared/success-message.blade.php
+++ resources/views/shared/success-message.blade.php
@@ -1,8 +1,3 @@
 @if (session()->has('success'))
-    <div class="bg-green-500 text-white font-bold px-4 py-2 rounded-md shadow-md transition-all duration-150 ease-in-out" role="alert">
-        {{ session('success') }}
-        <button type="button" class="top-0 right-0 px-3 py-1 text-white" data-dismiss="alert" aria-label="Close">X</button>
-    </div>
+    <div id="success-message" data-message="{{ session('success') }}"></div>
 @endif
-
-@vite('resources/js/app.js')
